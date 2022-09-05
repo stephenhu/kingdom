42f0c0916d10f7206bff6ffff8bad344f2dd1613
stephenhu <epynonymous@outlook.com>
2022-09-05 21:18:14 +0800

    sync code

---
+++ docs/design.md
@@ -0,0 +1,7 @@
+# design
+
+web interface for managing kingdoms
+
+* view current state of all kingdoms and respective municipals
+* modify certain policies like taxation, military draft age, public works projects, laws, war
+* see certain trends
---
+++ pages/theme.tsx
@@ -0,0 +1,16 @@
+import { ThemeOptions } from '@mui/material';
+
+export const themeOptions: ThemeOptions = {
+  palette: {
+    primary: {
+      main: '#000000',
+    },
+    secondary: {
+      main: '#000000',
+    },
+    background: {
+      default: '#000000',
+      paper: '#181818',
+    },
+  }
+};
